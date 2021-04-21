f37eebd1490093fb21d437b6adebc766063beb8f
Da_Vinchi <igselyak@gmail.com>
2021-04-21 04:26:26 +0300

    21.04.2021

--- headers.py
+++ headers.py
@@ -1,4 +1,4 @@
-headers = { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2MDdkOTZkYWYwM2U0NjAwMTFiMmU0YzEiLCJzdWIiOiI2MDQ5MGVmMzUzMDcyZWZjNGJmZDIyZTQiLCJncnAiOiI2MDQ5MGVmMzUzMDcyZWZjNGJmZDIyZTMiLCJsaWMiOmZhbHNlLCJ1c2ciOiJhcGkiLCJmdWxsIjpmYWxzZSwicmlnaHRzIjoxLjUsImlhdCI6MTYxODg0MzM1NCwiZXhwIjoxNjIxMzcxNjAwfQ.P2xRsPyZ9ZttCQllksyi-z5nx_NTG99tYdNfSjCxAfc",
+headers = { "Authorization": "Beraer token",
             "User Agent": 'Python/3.7',
             "Content-Type": "application/json",
             "Accept": "application/json",
